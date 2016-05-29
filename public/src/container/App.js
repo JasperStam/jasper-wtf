5d32dfec7512cd6dc5ad3c5b2570ce318a9b4fb4
JasperStam <jasper@strnk.nl>
2016-05-29 23:56:30 +0200

    Rename container

--- public/src/container/App.js
+++ public/src/container/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import Header from 'component/Header';
-import RadarChart from './RadarChart';
+import Rubrics from './Rubrics';
 import styles from './App.css';
 
 export default React.createClass({
     render() {
         return (
             <div className={styles.layout}>
-                <RadarChart />
+                <Rubrics />
             </div>
         );
     },
