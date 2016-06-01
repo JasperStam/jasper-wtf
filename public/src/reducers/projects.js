e4d12b8280b03f55c657cf0dc60726355730fe02
JasperStam <jasper@strnk.nl>
2016-06-01 22:47:35 +0200

    Add more projects

--- public/src/reducers/projects.js
+++ public/src/reducers/projects.js
@@ -1,12 +1,70 @@
 const base = [
     {
-        name: 'Address Based',
+        id: 1,
+        name: 'Flock',
+        description: '',
+        completed: true,
         categories: [
-            { name: 'Design', progress: 20 },
-            { name: 'Frontend', progress: 60 },
-            { name: 'Backend', progress: 50 },
-            { name: 'UI', progress: 40 },
-            { name: 'Algorithms', progress: 30 },
+            { name: 'Design', progress: 5 },
+            { name: 'UI', progress: 5 },
+        ],
+    }, {
+        id: 2,
+        name: 'WKdZ',
+        description: '',
+        completed: true,
+        categories: [
+            { name: 'Design', progress: 3 },
+            { name: 'Backend', progress: 5 },
+            { name: 'UI', progress: 5 },
+        ],
+    }, {
+        id: 3,
+        name: 'Address Based',
+        description: '',
+        completed: true,
+        categories: [
+            { name: 'Design', progress: 8 },
+            { name: 'Frontend', progress: 20 },
+            { name: 'Backend', progress: 10 },
+            { name: 'UI', progress: 10 },
+            { name: 'Algorithms', progress: 20 },
+        ],
+    }, {
+        id: 4,
+        name: 'RVI',
+        description: '',
+        completed: false,
+        categories: [
+            { name: 'Design', progress: 5 },
+            { name: 'Frontend', progress: 25 },
+            { name: 'Backend', progress: 3 },
+            { name: 'UI', progress: 10 },
+        ],
+    }, {
+        id: 5,
+        name: 'Rex',
+        description: '',
+        completed: true,
+        categories: [
+            { name: 'Frontend', progress: 5 },
+            { name: 'Backend', progress: 5 },
+        ],
+    }, {
+        id: 6,
+        description: '',
+        completed: false,
+        name: 'Dashboard',
+        categories: [
+            { name: 'Backend', progress: 5 },
+        ],
+    }, {
+        id: 7,
+        description: '',
+        completed: false,
+        name: 'Modus',
+        categories: [
+            { name: 'Backend', progress: 3 },
         ],
     },
 ];
