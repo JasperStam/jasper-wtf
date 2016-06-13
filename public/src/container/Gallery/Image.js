e61f836cf67213ebf553b163ce691d121fefcfac
JasperStam <jasper@strnk.nl>
2016-06-13 16:12:43 +0200

    Refactor the Gallery Image into a stateless function

--- public/src/container/Gallery/Image.js
+++ public/src/container/Gallery/Image.js
@@ -1,15 +1,14 @@
 import React, { PropTypes } from 'react';
 import styles from './Image.css';
 
-export default React.createClass({
-    propTypes: {
-        src: PropTypes.string.isRequired,
-    },
-    render() {
-        return (
-            <div>
-                <img src={this.props.src} className={styles.image} />
-            </div>
-        );
-    },
-});
+const Image = ({ src }) => (
+    <div>
+        <img src={src} className={styles.image} />
+    </div>
+);
+
+Image.propTypes = {
+    src: PropTypes.string.isRequired,
+};
+
+export default Image;
