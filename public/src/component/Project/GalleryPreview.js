d6d0394f3d16ed76ce2d498ef9f580b66fa90ffa
JasperStam <jasper@strnk.nl>
2016-06-13 15:44:06 +0200

    Show gallery on previewClick

--- public/src/component/Project/GalleryPreview.js
+++ public/src/component/Project/GalleryPreview.js
@@ -1,14 +1,15 @@
 import React, { PropTypes } from 'react';
 import styles from './Preview.css';
 
-const GalleryPreview = ({ previewUrl }) => (
+const GalleryPreview = ({ previewUrl, showGallery }) => (
     <div className={styles.imageContainer}>
-        <img src={`static/${previewUrl}`} className={styles.image} />
+        <img src={`static/${previewUrl}`} className={styles.image} onClick={showGallery} />
     </div>
 );
 
 GalleryPreview.propTypes = {
     previewUrl: PropTypes.string.isRequired,
+    showGallery: PropTypes.func.isRequired,
 };
 
 export default GalleryPreview;
--- public/src/container/Project/GalleryPreview.js
+++ public/src/container/Project/GalleryPreview.js
@@ -1,16 +1,35 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { showGallery } from 'actions/index';
 import GalleryPreview from 'component/Project/GalleryPreview';
 
-export default React.createClass({
+const Preview = React.createClass({
     propTypes: {
         images: PropTypes.arrayOf(PropTypes.string).isRequired,
+        dispatchGallery: PropTypes.func.isRequired,
     },
     getPreviewUrl() {
         return this.props.images[0];
     },
+    showGallery() {
+        this.props.dispatchGallery(this.props.images);
+    },
     render() {
         return (
-            <GalleryPreview previewUrl={this.getPreviewUrl()} { ... this.props } />
+            <GalleryPreview previewUrl={this.getPreviewUrl()} showGallery={this.showGallery}{ ... this.props } />
         );
     },
 });
+
+const mapStateToProps = () => ({ });
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatchGallery: (images) => {
+        dispatch(showGallery(images));
+    },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Preview);
